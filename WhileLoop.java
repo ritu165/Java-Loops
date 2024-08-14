9e467cf581be328913622e5a3afbf0641db47719
ritu165 <sarkarritu398@gmail.com>
2024-08-14 23:52:10 +0530

    initialization

---
+++ WhileLoop.java
@@ -0,0 +1,9 @@
+public class WhileLoop {
+    public static void main(String[] agrs) {
+        int count = 0;
+        while (count < 3) {
+            System.out.println("Hello, World!");
+            count++;
+        }
+    }
+}
